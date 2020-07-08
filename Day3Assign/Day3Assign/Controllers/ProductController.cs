27f78a570685b28214d5586b78bbc4834c1fcbfa
Vinayak Hamsagar <rishitb411@gmail.com>
2020-07-08 20:16:49 +0530

    First Commit

---
+++ Day3Assign/Day3Assign/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Day3Assign.Models;
+
+namespace Day3Assign.Controllers
+{
+    public class ProductController : Controller
+    {
+        public static List<Product> list = new List<Product>() {
+        new Product(){pid="1001", pname="Mobile", price=12000, stock=40},
+        new Product(){pid="1002", pname="TV", price=20000, stock=10}
+        };
+        public IActionResult Index()
+        {
+            return View(list);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product p)
+        {
+            list.Add(p);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Day3Assign/Day3Assign/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Day3Assign.Models
+{
+    public class Product
+    {
+        public string pid { get; set; }
+        public string pname { get; set; }
+        public int price { get; set; }
+        public int stock { get; set; }
+
+    }
+}
